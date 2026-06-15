478d59a031b7f2a972b22a48b636819d4ea17a52
Abdullah <abdullahsd030404@gmail.com>
2026-06-15 12:24:09 +0800

    add axios

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 3.  curl.exe http://localhost:5000/isbn/1
 4.  curl.exe "http://localhost:5000/author/Unknown"   
 5.  curl.exe "http://localhost:5000/title/Things-Fall-Apart"
-6.  
-7.
-8.
-9.
-10.
+6.  curl.exe http://localhost:5000/review/2
+7.  curl.exe -X POST -H "Content-Type: application/json" -d '{\"username\":\"john_doe\", \"password\":\"secret123\"}' http://localhost:5000/register
+8.  curl.exe -c cookie.txt -X POST -H "Content-Type: application/json" -d '{\"username\":\"john_doe\", \"password\":\"secret123\"}' http://localhost:5000/customer/login
+9.  curl.exe -b cookie.txt -X PUT "http://localhost:5000/customer/auth/review/1?review=Great%20book!"
+10. curl.exe -b cookie.txt -X DELETE http://localhost:5000/customer/auth/review/1
+11. 
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,45 +22,77 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
+  const getBooks = new Promise((resolve, reject) => {
+    resolve(books);
+  });
+  
+  getBooks.then(
+    (bookList) => res.status(200).send(JSON.stringify(bookList, null, 4)),
+    (err) => res.status(500).json({ message: "Error retrieving books" })
+  );
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  const getBook = new Promise((resolve, reject) => {
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject({ status: 404, message: "Book not found" });
+    }
+  });
+
+  getBook.then(
+    (book) => res.status(200).send(JSON.stringify(book, null, 4)),
+    (err) => res.status(err.status).json({ message: err.message })
+  );
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author.toLowerCase().replace(/-/g, ' ');
-  const bookList = [];
-  for (let isbn in books) {
-    if (books[isbn].author.toLowerCase() === author) {
-      bookList.push({
-        isbn: isbn,
-        title: books[isbn].title,
-        reviews: books[isbn].reviews
-      });
+  const getBooksByAuthor = new Promise((resolve, reject) => {
+    const bookList = [];
+    for (let isbn in books) {
+      if (books[isbn].author.toLowerCase() === author) {
+        bookList.push({
+          isbn: isbn,
+          title: books[isbn].title,
+          reviews: books[isbn].reviews
+        });
+      }
     }
-  }
-  return res.status(200).send(JSON.stringify({ booksbyauthor: bookList }, null, 4));
+    resolve(bookList);
+  });
+
+  getBooksByAuthor.then(
+    (bookList) => res.status(200).send(JSON.stringify({ booksbyauthor: bookList }, null, 4)),
+    (err) => res.status(500).json({ message: "Error retrieving books" })
+  );
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title.toLowerCase().replace(/-/g, ' ');
-  const bookList = [];
-  for (let isbn in books) {
-    if (books[isbn].title.toLowerCase() === title) {
-      bookList.push({
-        isbn: isbn,
-        author: books[isbn].author,
-        reviews: books[isbn].reviews
-      });
+  const getBooksByTitle = new Promise((resolve, reject) => {
+    const bookList = [];
+    for (let isbn in books) {
+      if (books[isbn].title.toLowerCase() === title) {
+        bookList.push({
+          isbn: isbn,
+          author: books[isbn].author,
+          reviews: books[isbn].reviews
+        });
+      }
     }
-  }
-  return res.status(200).send(JSON.stringify({ booksbytitle: bookList }, null, 4));
+    resolve(bookList);
+  });
+
+  getBooksByTitle.then(
+    (bookList) => res.status(200).send(JSON.stringify({ booksbytitle: bookList }, null, 4)),
+    (err) => res.status(500).json({ message: "Error retrieving books" })
+  );
 });
 
 //  Get book review
